21bf23e034d3a717a0550266e37f9d37842dfd58
scoyne12 <scoyne122@gmail.com>
2018-08-16 15:19:23 -0500

    background image

--- src/js/components/Login/Login.js
+++ src/js/components/Login/Login.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import NamePanel from './NamePanel';
 
+const styles = {
+    login: {
+        width: '100vw',
+        height: '100vh',
+        backgroundImage: 'url("https://media.giphy.com/media/BlOY9MQiFKqIg/giphy.gif")'
+    }
+}
+
 class Login extends React.Component {
 
 	render() {
 		return (
-			<NamePanel />
+		    <div style={styles.login}>
+			    <NamePanel />
+            </div>
 		);
 	}
 
