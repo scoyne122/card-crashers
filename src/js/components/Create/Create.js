5cade2beebe56a7b8a64a82c134c46f990221c43
scoyne12 <scoyne122@gmail.com>
2018-08-16 16:42:18 -0500

    navigate routes via buttons

--- src/js/components/Create/Create.js
+++ src/js/components/Create/Create.js
@@ -4,24 +4,24 @@ import Input from '../common/Input';
 import Button from '../common/Button';
 
 const styles = {
-    create: {
-        width: '6cm',
-        height: '100vh',
-    }
+	create: {
+		width: '6cm',
+		height: '100vh',
+	}
 };
 
 class Create extends React.Component {
 
-    render() {
-        return (
-            <div style={styles.create}>
-                <h2>Create a game</h2>
-                <Input placeholder='Choose a name'/>
-                <h4>Your passcode is: abc123</h4>
-                <Button text='got it!'/>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div style={styles.create}>
+				<h2>Create a game</h2>
+				<Input placeholder='Choose a name'/>
+				<h4>Your passcode is: abc123</h4>
+				<Button text='got it!'/>
+			</div>
+		);
+	}
 
 }
 
