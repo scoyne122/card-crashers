4e8d34a7fc75dc85d309556ace6bd974fb429e25
scoyne12 <scoyne122@gmail.com>
2018-08-16 16:42:32 -0500

    navigate routes via buttons

--- src/js/components/Join/Join.js
+++ src/js/components/Join/Join.js
@@ -4,24 +4,24 @@ import Input from '../common/Input';
 import Button from '../common/Button';
 
 const styles = {
-    join: {
-        width: '6cm',
-        height: '100vh',
-    }
+	join: {
+		width: '6cm',
+		height: '100vh',
+	}
 };
 
 class Join extends React.Component {
 
-    render() {
-        return (
-            <div style={styles.join}>
-                <h2>Join a game</h2>
-                <Input placeholder='Enter game code'/>
-                <br/><br/>
-                <Button text='Go!'/>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div style={styles.join}>
+				<h2>Join a game</h2>
+				<Input placeholder='Enter game code'/>
+				<br/><br/>
+				<Button text='Go!'/>
+			</div>
+		);
+	}
 
 }
 
