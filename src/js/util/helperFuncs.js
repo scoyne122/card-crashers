d0c03ed4bed27daa15353ff49f298a7b138b2d33
Scott Coyne <ScottC@scotts-air-2.lan>
2018-08-17 15:10:51 -0500

    lint fix helperFuncs

--- src/js/util/helperFuncs.js
+++ src/js/util/helperFuncs.js
@@ -3,8 +3,8 @@
  * @param {number} size - length of the id
  */
 export function makeid(size = 5) {
-	var text = "";
-	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	var text = '';
+	var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   
 	for (var i = 0; i < size; i++)
 	  text += possible.charAt(Math.floor(Math.random() * possible.length));
